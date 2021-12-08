0a0f4c2ca32a732ec46a248a77f9e5b6c21ad5fd
alexthegreat194 <alexander.gentry@aol.com>
2021-12-07 22:56:07 -0800

    created templates

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
-    return redirect("www.google.com")
+    return render_template("index.html")
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template("dashboard.html")
+
+@app.route('/login')
+def login():
+    return render_template("login.html")
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
