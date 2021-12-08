16c20e68cd528e5feaf4f6f89449b21f6973975e
alexthegreat194 <alexander.gentry@aol.com>
2021-12-07 23:27:42 -0800

    added pymango

--- app.py
+++ app.py
@@ -1,20 +1,27 @@
 from flask import Flask, redirect, url_for, render_template
+from pymongo import MongoClient
 
 app = Flask(__name__)
+
+#pymango setup
+client = MongoClient()
+users = client.users
+classes = client.classes
+assignments = client.assignments
 
 @app.route('/', methods=['GET'])
 def index():
     return render_template("index.html")
 
-@app.route('/dashboard')
+@app.route('/dashboard', methods=['GET'])
 def dashboard():
     return render_template("dashboard.html")
 
-@app.route('/login')
+@app.route('/login', methods=['GET'])
 def login():
     return render_template("login.html")
 
-@app.route('/signup')
+@app.route('/signup', methods=['GET'])
 def signup():
     return render_template('signup.html')
 
