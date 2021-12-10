fc187a5caade3dffb4d16160405bc14301e645d8
alexthegreat194 <alexander.gentry@aol.com>
2021-12-10 15:30:06 -0800

    make secret key secret

--- app.py
+++ app.py
@@ -5,8 +5,7 @@ from pymongo import MongoClient
 from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
-# app.secret_key = os.urandom(12)
-app.secret_key = 'funny_key'
+app.secret_key = os.environ.get('SECRET_KEY')
 
 #Bcrypt setup
 bcrypt = Bcrypt(app)
